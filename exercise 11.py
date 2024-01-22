7ce5da27550c21fee0a3e809fb70207a81299811
johnmichaelpplale <johnmichaelpplale@gmail.com>
2024-01-22 16:20:16 +0800

    print and return

--- exercise 11.py
+++ exercise 11.py
@@ -2,3 +2,6 @@
 def reverse(number):
     # assign a variable that will make the number reverse with space
     reverse = ' '.join(str(number)[::-1])
+    # print the reverse
+    print(reverse)
+    return True
